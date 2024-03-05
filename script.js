abdc23fa84dc0643c7e17ae2c26990c638b8efd6
lajkogege <lajkogege@gmail.com>
2024-03-05 17:27:21 +0100

    elso feldatig

--- script.js
+++ script.js
@@ -6,9 +6,13 @@ window.addEventListener("load", function () {
 
 function elemekElerese1() {
     /**Ide jön az első feladat */
+    ELEM=document.querySelectorAll("section h2")[0]
+    console.log(ELEM);
 }
 function elemekElerese2() {
     /**Ide jön az 2.  feladat */
+    const ELEM=document.getElementByID("ide")
+    
 }
 function elemekElerese3() {
     /**Ide jön az 3. feladat */
