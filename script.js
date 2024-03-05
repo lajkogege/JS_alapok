1d928f4dcc5114cb727ba37648ab782c826f9ed4
lajkogege <lajkogege@gmail.com>
2024-03-05 17:58:25 +0100

    masoidk, harmadik feldat

--- script.js
+++ script.js
@@ -6,14 +6,19 @@ window.addEventListener("load", function () {
 
 function elemekElerese1() {
     /**Ide jön az első feladat */
-    ELEM=document.querySelectorAll("section h2")[0]
-    console.log(ELEM);
+    ELEM1=document.querySelectorAll("section h2")[0]
+    console.log(ELEM1);
 }
 function elemekElerese2() {
     /**Ide jön az 2.  feladat */
-    const ELEM=document.getElementByID("ide")
+    const ELEM=document.querySelectorAll(".ide")[0];
+    ELEM2.innerHTML = "<p>Jó reggelt! <p>";
+
     
 }
 function elemekElerese3() {
     /**Ide jön az 3. feladat */
+    ELEM3=document.getElementsByClassName("ide");
+    ELEM3.innerHTML="<p>Jó reggelt! <p>";
 }
+
